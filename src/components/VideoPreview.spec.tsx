963c04c8f5af3987876ee57a02e4a40f9bbd5a09
petro_kash <petro@kash.io>
2022-10-06 15:19:26 +0300

    Add video preview to sidebar

--- src/components/VideoPreview.spec.tsx
+++ src/components/VideoPreview.spec.tsx
@@ -1,12 +1,16 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import VideoPreview from './VideoPreview'
+
+jest.mock('video.js', () => () => ({ src: () => jest.fn() }))
 
 const props: any = {
   sdk: {
     entry: {
       fields: {
-        videoFile: {
-          getValue: () => null
+        videoUrls: {
+          getValue: () => ({
+            'application/x-mpegURL': 'url'
+          })
         }
       }
     },
@@ -23,12 +27,6 @@ describe('VideoPreview component', () => {
       it('should call startAutoResizer', () => {
         render(<VideoPreview {...props} />)
         expect(props.sdk.window.startAutoResizer).toHaveBeenCalled()
-      })
-      describe('when videoFile is not attached to story', () => {
-        it('should render title', () => {
-          render(<VideoPreview {...props} />)
-          expect(screen.getByText(/Video is not attached to this story/i)).toBeTruthy()
-        })
       })
     })
   })
--- src/components/VideoPreview.tsx
+++ src/components/VideoPreview.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { SidebarExtensionSDK } from '@contentful/app-sdk'
 import videojs from 'video.js'
 import 'video.js/dist/video-js.css'
 import 'videojs-contrib-hls'
-import { Note } from '@contentful/f36-components'
 
 export type Props = {
   sdk: SidebarExtensionSDK
@@ -11,34 +10,18 @@ export type Props = {
 
 const VideoPreview = (props: Props) => {
   const { sdk } = props
-  const [isVideoAvailable, setVideoAvailable] = useState(false)
 
   useEffect(() => {
-    sdk.window.startAutoResizer()
-    const videoId = sdk.entry.fields.videoFile.getValue()?.sys?.id
-    if (!videoId) {
-      return
-    }
-    setVideoAvailable(true)
-    sdk.space.getEntry(videoId).then((data: any) => {
-      const player = videojs('video-preview')
-      const videoUrls = data.fields.videoUrls['en-US']
-      player.src({
-        src: videoUrls['application/x-mpegURL'] ? videoUrls['application/x-mpegURL'] : videoUrls['video/mp4'],
-        type: videoUrls['application/x-mpegURL'] ? 'application/x-mpegURL' : 'video/mp4'
-      })
+    const player = videojs('video-preview')
+    const videoUrls = sdk.entry.fields.videoUrls.getValue()
+    player.src({
+      src: videoUrls['application/x-mpegURL'] ? videoUrls['application/x-mpegURL'] : videoUrls['video/mp4'],
+      type: videoUrls['application/x-mpegURL'] ? 'application/x-mpegURL' : 'video/mp4'
     })
+    sdk.window.startAutoResizer()
   }, [sdk])
 
-  return (
-    <>
-      {isVideoAvailable ? (
-        <video id='video-preview' width='310' height='250' className='video-js' controls />
-      ) : (
-        <Note variant='neutral'>Video is not attached to this story</Note>
-      )}
-    </>
-  )
+  return <video id='video-preview' width='310' height='250' className='video-js' controls />
 }
 
 export default VideoPreview
