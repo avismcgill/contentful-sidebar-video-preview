d6c3b10889e12ac278f345bfc584ff2efccfca61
petro_kash <petro@kash.io>
2022-10-07 09:14:20 +0300

    Fixes after code review

--- src/components/VideoPreview.spec.tsx
+++ src/components/VideoPreview.spec.tsx
@@ -2,6 +2,8 @@ import { render } from '@testing-library/react'
 import VideoPreview from './VideoPreview'
 
 jest.mock('video.js', () => () => ({ src: () => jest.fn() }))
+jest.mock('videojs-contrib-quality-levels', () => jest.fn())
+jest.mock('videojs-hls-quality-selector', () => jest.fn())
 
 const props: any = {
   sdk: {
--- src/components/VideoPreview.tsx
+++ src/components/VideoPreview.tsx
@@ -2,7 +2,8 @@ import { useEffect } from 'react'
 import { SidebarExtensionSDK } from '@contentful/app-sdk'
 import videojs from 'video.js'
 import 'video.js/dist/video-js.css'
-import 'videojs-contrib-hls'
+import 'videojs-contrib-quality-levels'
+import videojsqualityselector from 'videojs-hls-quality-selector'
 
 type Props = {
   sdk: SidebarExtensionSDK
@@ -12,16 +13,23 @@ const VideoPreview = (props: Props) => {
   const { sdk } = props
 
   useEffect(() => {
-    const player = videojs('video-preview')
+    const player = videojs('video-preview', {
+      controls: true,
+      controlBar: {
+        subsCapsButton: false
+      }
+    })
     const videoUrls = sdk.entry.fields.videoUrls.getValue()
     player.src({
       src: videoUrls['application/x-mpegURL'] ? videoUrls['application/x-mpegURL'] : videoUrls['video/mp4'],
       type: videoUrls['application/x-mpegURL'] ? 'application/x-mpegURL' : 'video/mp4'
     })
+    player.hlsQualitySelector = videojsqualityselector
+    player.hlsQualitySelector()
     sdk.window.startAutoResizer()
   }, [sdk])
 
-  return <video id='video-preview' className='video-js vjs-default-skin vjs-big-play-centered vjs-16-9' controls />
+  return <video id='video-preview' className='video-js vjs-big-play-centered vjs-fluid' />
 }
 
 export default VideoPreview
